e6458807b0cfe10b7abb0889231e06cef842bba8
puddo <davifpuddo@gmail.com>
2026-06-15 13:25:06 -0300

    comments + random string for LCS

--- DP/LCS.c
+++ DP/LCS.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 char* LCS (char*** table, char* x, char* y, int n, int m) {
 
     char* res = "";
     if (n > 0 && m > 0) {
+
+        // Current pos
         char** cur = &table[n-1][m-1];
 
+        // Reuse the value if possible
         if (*cur) {
             res = *cur;
 
         } else {
+
+            // New character
             res = (char*)calloc(2, sizeof(char)); // 1 char + \0
 
-            char cx = x[n-1];
-            char cy = y[m-1];
+            char cx = x[n-1];    // Last of x
+            char cy = y[m-1];    // Lasf of y
 
             if (cx == cy) {
                 res[0] = cx;
                 strcat(res, LCS(table, x, y, n-1, m-1));
 
             } else {
-               char* t1 = LCS(table, x, y, n-1, m);
-               char* t2 = LCS(table, x, y, n, m-1);
 
-               if (strlen(t1) > strlen(t2)) {
+               char* t1 = LCS(table, x, y, n-1, m);  // Reduce x
+               char* t2 = LCS(table, x, y, n, m-1);  // Reduce y
+
+               if (strlen(t1) > strlen(t2)) {   // Maximize size
                     strcat(res, t1);
                } else {
                     strcat(res, t2);
@@ -52,13 +59,27 @@ void invertS (char* x) {
     }
 }
 
-int main (void) {
-    
-    char x [6] = "12345";
-    char y [6] = "92349";
+char* randStr (int n) {
+    char* res = (char*)calloc(n+1, sizeof(char));
+    for (int i = 0; i < n; i++) {
+        int r = rand() % 26;
+        res[i] = 'A' + r;
+    }
+    return res;
+}
 
-    int n = 5;
-    int m = 5;
+int main (void) {
+
+    srand(time(NULL));
+    
+    char* x = randStr(7);
+    char* y = randStr(7);
+
+    printf("x: %s\n", x);
+    printf("y: %s\n", y);
+
+    int n = 7;
+    int m = 7;
 
     char*** table = (char***)calloc(n,sizeof(char**));
 
